deb9807a75e6d1246475c52a0f6cfdffbe5eef4a
Logan Sorensen <loganmsorensen@gmail.com>
2020-02-13 15:08:30 -0800

    commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // code away!
-require('dotenv').config();
+require("dotenv").config();
 
-const server = require('./server.js');
+const server = require("./server.js");
+
+server.get("/", (req, res) => {
+  res.send(`
+    <h2>Deployed API</h2>
+    `);
+});
 
 const port = process.env.PORT || 5001;
 server.listen(port, () => {
-    console.log(`\n* Server running on http://localhost:${port} *\n`)
-})
+  console.log(`\n* Server running on http://localhost:${port} *\n`);
+});
